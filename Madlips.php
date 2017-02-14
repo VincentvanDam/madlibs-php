4f51a73a900d253ae0c5a7323230be40262d0685
Vincent van Dam <99027519@mydavinci.nl>
2017-02-14 11:52:53 +0100

    no message

---
+++ Madlips.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Madlips</title>
+	<link rel="stylesheet" type="text/css" href="Madlips.css">
+	<meta charset="utf-8">
+</head>
+<body>
+<form>
+<p>Welk dier zou je nooit als huisdier willen hebben?</p><input type="text" name="name">
+<p>Welk dier zou je nooit als huisdier willen hebben?</p><input type="text" name="name">
+</form>
+
+</body>
+</html>
