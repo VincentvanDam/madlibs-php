c7a68858cf8327defd92839d463e77bd55d30baa
Vincent van Dam <99027519@mydavinci.nl>
2017-02-20 15:03:01 +0100

    no message

---
+++ ErHeerstPaniek.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Madlibs</title>
+	<link rel="stylesheet" type="text/css" href="Madlibs.css">
+	<meta charset="utf-8">
+</head>
+<body>
+<h1>Mad Libs</h1>
+</div>
+<h2>Er heerst paniek...</h2>
+<form action="ErHeerstPaniekVerhaal.php" method="post">
+		Welk dier zou je nooit als huisdier willen hebben?
+		<input type="text" name="Input1"><br>
+		Wie is de belangrijkste in je leven?
+		<input type="text" name="Input2"><br>
+		In welk land zou je graag willen wonen?
+		<input type="text" name="Input3"><br>
+		Wat doe je als je je verveelt?
+		<input type="text" name="Input4"><br>
+		Met welk speelgoed speelde je als kind het meest?
+		<input type="text" name="Input5"><br>
+		Bij welke docent spijbel je het liefst?
+		<input type="text" name="Input6"><br>
+		Als je €100.000,- had, wat zou je dan kopen?
+		<input type="text" name="Input7"><br>
+		Wat is je favoriete bezigheid?
+		<input type="text" name="Input8"><br>
+		<input type="submit" value="send">
+</form>
+</body>
+</html>
---
+++ ErHeerstPaniekVerhaal.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MadLibs</title>
+</head>
+<body>
+<h2>Er heerst paniek...</h2>
+<p>Er heerst paniek in het koningkrijk <?php echo $_POST["Input3"]; ?>, Koning <?php echo $_POST["Input6"]; ?> is ten einde raad en als koning <?php echo $_POST["Input6"]; ?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?php echo $_POST["Input2"]; ?></p>
+
+<p>"<?php echo $_POST["Input2"]; ?>! Het is een ramp! Het is een schande!"</p>
+
+<p>"Mijn <?php echo $_POST["Input1"]; ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["Input5"]; ?> voor hem gekocht!</p>
+
+<p>"Majesteit, uw <?php echo $_POST["Input1"]; ?> komt vast vanzelf weer terug?"</p>
+
+<p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd "<?php echo $_POST["Input8"]; ?> leren?"</p>
+
+<p>"Maar Sire, daar kunt u toch uw <?php echo $_POST["Input7"]; ?> voor gebruiken"</p>
+
+<p>"<?php echo $_POST["Input2"]; ?> je hebt helemaal gelijk! Wat zou ik doen las ik jou niet had"</p>
+
+<p>"<?php echo $_POST["Input4"]; ?>, Sire"</p>
+</body>
+</html>
